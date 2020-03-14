54f097d533a4d93c3795aead03b40cea508bbe08
djeck1432 <djeck1432@users.noreply.github.com>
2020-03-15 00:07:45 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-if __name__=='__main__':
-    response = requests.post('https://itproger.com/news/76')
-    soup = BeautifulSoup(response.content)
-
-
-    print(soup.div['block'])
